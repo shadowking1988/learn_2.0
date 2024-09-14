cc755b3ef86298a8762dd29b45316ff7d4b92738
shadowking1988 <soumalya.g88@gmail.com>
2024-09-14 10:55:24 +0530

    new changes

---
+++ rolleroaster.py
@@ -0,0 +1,8 @@
+print("Welcome to the Roller Coaster!")
+height = int(input("What is your height in cm? "))  # ask user for height in cm
+
+if  height >= 120:  # if height is greater than or equal to 120 cm
+    print("You can ride the roller coaster!")   # print that you can ride the roller coaster
+else:  # if height is less than 120 cm
+    print("Sorry, you have to grow taller before you can ride.")  # print that you have to grow taller before you can ride
+    print("Better luck next time!")  # print that better luck next time
