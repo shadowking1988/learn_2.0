ff4baefc8bf093de504ab231cc1a0732455f322f
shadowking1988 <soumalya.g88@gmail.com>
2024-09-15 05:09:01 +0530

    new changes

--- pizza_delivery_program.py
+++ pizza_delivery_program.py
@@ -1,7 +1,7 @@
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
+size = input("What size pizza do you want? S, M, or L ").upper()
+add_pepperoni = input("Do you want pepperoni? Y or N ").upper()
+extra_cheese = input("Do you want extra cheese? Y or N ").upper()
 bill = 0
 if size == "S":
     bill += 15
